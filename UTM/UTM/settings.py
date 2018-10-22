8730b71ddf2e0ee00847d1714e40d3084d720368
jcdepoppe <jcdepoppe@comcast.net>
2018-10-22 14:38:52 -0700

    Log in and registration

--- UTM/UTM/settings.py
+++ UTM/UTM/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.log_reg',
+    # 'apps.Textbooks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
