1a03f7332f83cbfda2992deaa4e1ebba3eaf7f1f
Nano Tellez <nanotellez@gmail.com>
2018-10-24 10:38:08 -0700

    Fixed the urls. New routes are working! (Thanks, Vidisha!)

--- UTM/apps/Textbooks/urls.py
+++ UTM/apps/Textbooks/urls.py
@@ -8,13 +8,13 @@ urlpatterns = [
 	url(r'^sell/(?P<id>\d+)/show$', views.show_sell),
 	url(r'^sell$', views.sell_book),
 	url(r'^sell/process$', views.sell_book_process),
-	url(r'^sell(?P<id>\d+)/edit$', views.edit_sell),
-	url(r'^sell(?P<id>\d+)/delete$', views.delete_sell),
+	url(r'^sell/(?P<id>\d+)/edit$', views.edit_sell),
+	url(r'^sell/(?P<id>\d+)/delete$', views.delete_sell),
 	######## BOOKS WISHLIST ##############
 	url(r'^want/(?P<id>\d+)/show$', views.show_want),
 	url(r'^want$', views.want_book),
 	url(r'^want/process$', views.want_book_process),
-	url(r'^want(?P<id>\d+)/edit$', views.edit_want),
-	url(r'^want(?P<id>\d+)/delete$', views.delete_want),
+	url(r'^want/(?P<id>\d+)/edit$', views.edit_want),
+	url(r'^want/(?P<id>\d+)/delete$', views.delete_want),
 	
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
